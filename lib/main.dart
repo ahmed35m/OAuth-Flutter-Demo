4c8d7d3228c31017f8f81a040e8ca7e2720d504d
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2021-06-15 16:41:51 -0600

    Added UI and Auth components

---
+++ lib/main.dart
@@ -0,0 +1,263 @@
+/// -----------------------------------
+///          External Packages
+/// -----------------------------------
+
+import 'package:flutter/material.dart';
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final FlutterAppAuth appAuth = FlutterAppAuth();
+final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+/// -----------------------------------
+///           Auth0 Variables
+/// -----------------------------------
+const AUTH0_DOMAIN = '';
+const AUTH0_CLIENT_ID = '';
+
+const AUTH0_REDIRECT_URI = 'com.auth0.flutterdemo://login-callback';
+const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
+
+/// -----------------------------------
+///           Profile Widget
+/// -----------------------------------
+
+class Profile extends StatelessWidget {
+  final logoutAction;
+  String? name;
+  String picture;
+  // picture=  (name != null) ? "123.png";
+  var p =
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canva.com%2Flogos%2Ftemplates%2F&psig=AOvVaw3buccAb2MgGzlUMfi9uzDF&ust=1623777550529000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjYrvTQl_ECFQAAAAAdAAAAABAD';
+  Profile(this.logoutAction,
+      {this.name, this.picture = 'https://picsum.photos/250?image=9'});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 4.0),
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(picture),
+            ),
+          ),
+        ),
+        SizedBox(height: 24.0),
+        Text('Name: $name'),
+        SizedBox(height: 48.0),
+        ElevatedButton(
+          onPressed: () {
+            logoutAction();
+          },
+          child: Text('Logout'),
+        ),
+      ],
+    );
+  }
+}
+
+/// -----------------------------------
+///            Login Widget
+/// -----------------------------------
+
+class Login extends StatelessWidget {
+  final loginAction;
+  final String? loginError;
+
+  const Login(this.loginAction, this.loginError);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            loginAction();
+          },
+          child: Text('Login'),
+        ),
+        // Text('Error occured in Login'),
+      ],
+    );
+  }
+}
+
+/// -----------------------------------
+///                 App
+/// -----------------------------------
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+/// -----------------------------------
+///              App State
+/// -----------------------------------
+
+class _MyAppState extends State<MyApp> {
+  bool isBusy = false;
+  bool isLoggedIn = false;
+  String? errorMessage;
+  String? name = "no one";
+  String? picture = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth0 Demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Auth0 Demo'),
+          ),
+          body: Column(children: [
+            (Center(
+              child: isBusy
+                  ? CircularProgressIndicator()
+                  : isLoggedIn
+                      ? Profile(logoutAction, name: name)
+                      : Login(loginAction, errorMessage),
+            )),
+            Text('Logged In Status: ${isLoggedIn}')
+          ])),
+    );
+  }
+
+  Map<String, dynamic> parseIdToken(String? idToken) {
+    if (idToken == null) {
+      idToken = 'Test';
+    }
+
+    final parts = idToken.split(r'.');
+    assert(parts.length == 3);
+
+    return jsonDecode(
+        utf8.decode(base64Url.decode(base64Url.normalize(parts[1]))));
+  }
+
+  Future<Map<String, dynamic>> getUserDetails(String? accessToken) async {
+    final Uri url = Uri.parse('https://$AUTH0_DOMAIN/userinfo');
+    final response = await http.get(
+      url,
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to get user details');
+    }
+  }
+
+  ////Log In Action
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      final AuthorizationTokenResponse? result =
+          await appAuth.authorizeAndExchangeCode(
+        AuthorizationTokenRequest(
+          AUTH0_CLIENT_ID,
+          AUTH0_REDIRECT_URI,
+          issuer: 'https://$AUTH0_DOMAIN',
+          scopes: ['openid', 'profile', 'offline_access'],
+          // promptValues: ['login']
+        ),
+      );
+      print(result!.accessTokenExpirationDateTime);
+      final idToken = parseIdToken(result.idToken);
+      print(idToken);
+      print(result.accessToken);
+
+      final profile = await getUserDetails(result.accessToken);
+
+      await secureStorage.write(
+          key: 'refresh_token', value: result.refreshToken);
+
+      setState(() {
+        isBusy = false;
+        isLoggedIn = true;
+        print("Login Status ${isLoggedIn}");
+
+        name = idToken['name'];
+        picture = profile['picture'];
+      });
+    } catch (e, s) {
+      print('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        isLoggedIn = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  void logoutAction() async {
+    await secureStorage.delete(key: 'refresh_token');
+    setState(() {
+      isLoggedIn = false;
+      isBusy = false;
+    });
+  }
+
+  @override
+  void initState() {
+    initAction();
+    super.initState();
+  }
+
+  void initAction() async {
+    final storedRefreshToken = await secureStorage.read(key: 'refresh_token');
+    // ignore: unnecessary_null_comparison
+    if (storedRefreshToken == null) return;
+
+    setState(() {
+      isBusy = true;
+    });
+
+    try {
+      final response = await appAuth.token(TokenRequest(
+        AUTH0_CLIENT_ID,
+        AUTH0_REDIRECT_URI,
+        issuer: AUTH0_ISSUER,
+        refreshToken: storedRefreshToken,
+      ));
+
+      if (response != null) {
+        final idToken = parseIdToken(response.idToken);
+
+        final profile = await getUserDetails(response.accessToken);
+
+        secureStorage.write(key: 'refresh_token', value: response.refreshToken);
+
+        setState(() {
+          isBusy = false;
+          isLoggedIn = true;
+          name = idToken['name'];
+          picture = profile['picture'];
+        });
+      }
+    } catch (e, s) {
+      print('error on refresh token: $e - stack: $s');
+      logoutAction();
+    }
+  }
+}
